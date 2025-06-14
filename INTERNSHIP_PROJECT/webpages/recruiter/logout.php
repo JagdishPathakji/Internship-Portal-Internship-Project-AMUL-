9693fea77153d12a633889645c22ec7d67a4b598
Jagdish Pathakji <jagdishgithub1@gmail.com>
2025-06-14 10:24:49 +0530

    Delete INTERNSHIP_PROJECT directory

--- INTERNSHIP_PROJECT/webpages/recruiter/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start(); 
-    session_unset(); 
-    session_destroy(); 
-    echo "<script> alert('Logout successfull'); </script>";
-    header("Location: ../../index.php"); 
-    exit();
-?>
